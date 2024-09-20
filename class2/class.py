ed36b26ead12b9d2c94c442bf71e3f08b47a33ea
DMChanCool <dmchan008@gmail.com>
2024-09-20 20:32:29 +0800

    class2

--- class2/class.py
+++ class2/class.py
@@ -1,1 +1,26 @@
 print("hello")
+# 字串運算
+a = "hello"
+b = "world"
+c = a + " " + b  # 字串加法
+print(c)
+
+a = a * 3  # 字串乘法
+print(a)
+
+# 認識基本指令
+# 指令會有名稱跟括號組成，()裡面放提供給指令的參數
+# 每個參數都要用逗號隔開
+m = max(1, 2, 3, 4, 5)  # 取最大值
+print(m)
+
+L = len("hello")  # 字串長度
+print(L)
+
+T = type("hello")  # 字串類型
+print(T)
+
+print(type(1))  # 取得參數型態
+print(type(1.0))  # 取得參數型態
+print(type("hello"))  # 取得參數型態
+print(type(True))  # 取得參數型態
