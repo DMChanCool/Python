f31897fe6dac5b061ccf8d9ff546e21de2a7fe20
DMChanCool <dmchan008@gmail.com>
2024-09-20 20:08:09 +0800

    訂正hw1

---
+++ class1/prj1.py
@@ -0,0 +1,18 @@
+print("Hello World")  # 印字串的指令
+# 這是單行註解
+"""
+這是多行註解1
+這是多行註解2
+這是多行註解3
+"""
+# 基本型態
+print(1)  # 整數int
+print(1.0)  # 浮點數float, 只要有小數點就是浮點數
+print("Hello World")  # 字串 str, 用雙引號""或單引號''包起來
+print(True)  # 布林值 bool, 第一個字母要大寫
+print(None)  # 空值 Nonetype
+
+# 變數 variable
+a = 1  # 把1這個整數存到電腦的記憶體中，並取名為a
+# =的功能是將右邊的資料存到左邊的變數空間當中
+print(a)  # 把a這個變數得值印出來
