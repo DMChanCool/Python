d58b43ed5ff934f8d2ec9791fa1e5d28fa2d75e3
DMChanCool <dmchan008@gmail.com>
2024-10-25 19:29:30 +0800

    class6 hw

--- class6/hw.py
+++ class6/hw.py
@@ -1,20 +1,22 @@
 import turtle as t
 import time
 
-t.speed(9999999999999999999999999999999999999999999999999999999999999999999)
+t.speed(0)
 
-for i in range(61):
-    t.penup()
-    for u in range(12):
+for i in range(60):
+    for u in range(1, 13):
+        t.penup()
         t.forward(300)
         t.stamp()
         t.backward(300)
         t.right(30)
+    print(6 * i)
+    t.right(6 * i)
     t.pendown()
-    t.right(6)
     t.forward(250)
     t.backward(250)
     time.sleep(1)  # sleep 1 seconds
     t.clear()
+    t.left(6 * i)
 
 t.done()
