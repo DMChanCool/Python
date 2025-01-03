46c1bd9231b86bc2bc54e398149a9513f7c6a42c
DMChanCool <dmchan008@gmail.com>
2025-01-03 20:34:00 +0800

    class17

--- class16/hw.py
+++ class16/hw.py
@@ -1,3 +1,38 @@
+def add():
+    global score_dict
+    subject = input("請輸入想新增的科目:")
+    if subject in score_dict:
+        print("此科目已經新增過囉!")
+    else:
+        while True:
+            try:
+                score = int(input("請輸入分數:"))
+                score_dict[subject] = score
+                break
+            except:
+                print("輸入錯誤，請重新輸入")
+
+
+def delete():
+    global score_dict
+    subject = input("請輸入想刪除的科目:")
+    if subject in score_dict:
+        score_dict.pop(subject)
+        print("刪除成功!")
+    else:
+        print("此科目尚未新增!")
+
+
+def exit():
+    global score_dict
+    if len(score_dict) == 0:
+        print("目前沒有登記成績喔!")
+    else:
+        for subject, score in score_dict.items():
+            print(f"{subject}:{score}")
+        print(f"總平均為:{sum(score_dict.values())/len(score_dict)}")
+
+
 def edit():
     global score_dict
     subject = input("請輸入想修改的科目:")
--- class17/class.py
+++ class17/class.py
@@ -7,3 +7,35 @@ a = 10
 b = a
 b = 20  # 修改b的值，但是不會影響a
 print(a)  # shows 10
+
+
+# def doc用途
+def add(a, b):
+    """
+    加法函式
+    :param a:數字1
+    :param b:數字2
+    :return:a+b
+    """
+    return a + b
+
+
+# using the "help" function you can view the function's documentation
+# ex:
+# help(add)
+
+# you can also use the __doc__ attribute to view the function's documentation
+# ex:
+print(add.__doc__)
+
+
+# eval(input()) -this function allows the user's input text to become 程式碼
+# ex:
+ans = eval("5+3")
+print(ans)  # shows 8
+
+# this way, users can input mathematical expressions and caculate the result
+# ex:
+ans = eval(input("輸入算式:"))
+print(ans)
+# 5+3--->8
